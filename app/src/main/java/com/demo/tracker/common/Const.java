142c8354362f3340e2c52795b7a096feb1e04f5d
Niladree Bhattacharjee <niladreeb@gmail.com>
2016-07-12 15:50:42 +0100

    Update Const.java
    Updated demo api details

--- app/src/main/java/com/demo/tracker/common/Const.java
+++ app/src/main/java/com/demo/tracker/common/Const.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Const {
-
-    public static final String API_ENDPOINT = "https://www.wunelliuat.com/testingtalent";
-    public static final String API_JOURNEYS = "/journeys";
-    public static final String API_LOCATION = "/user/location";
-    public static final String USERNAME = "WunelliTestUser";
-    public static final String PASSWORD = "resUtseTillenuW";
+    
+    //demo details
+    public static final String API_ENDPOINT = "url";   
+    public static final String API_JOURNEYS = "/details";
+    public static final String API_LOCATION = "/details";
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
 
     public enum BundleKey {
         COORDINATE,
